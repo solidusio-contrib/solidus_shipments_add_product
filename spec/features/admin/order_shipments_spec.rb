3d062e044c200426bc5a3ce9b9eb11353b513dc2
andrea longhi <andrea@spaghetticode.it>
2019-08-30 11:27:24 +0200

    Add integration spec for admin order page

---
+++ spec/features/admin/order_shipments_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Order shipments page', type: :feature do
+  stub_authorization!
+
+  let!(:order) { create :order_ready_to_ship }
+  let!(:product) { create(:product_in_stock) }
+
+  before do
+    visit spree.edit_admin_order_path(order)
+  end
+
+  it 'can add an item from the shipments order page', :js do
+    within('table.shipment') do
+      expect(page).not_to have_content product.name
+    end
+
+    find('.variant_autocomplete').click
+    fill_in 's2id_autogen1_search', with: product.name
+    find('.variant-autocomplete-item').click
+    find('.add_variant').click
+
+    within('table.shipment') do
+      expect(page).to have_content product.name
+    end
+  end
+end
